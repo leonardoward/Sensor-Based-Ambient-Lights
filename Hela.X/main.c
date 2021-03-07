61730ca2258ab59da78b7cc606d0a90c5f7334c4
carlos <wuopcarlos@gmail.com>
2021-03-07 13:56:54 -0400

    FEAT: init of the function capable of transforming hour to datetime

--- Hela.X/main.c
+++ Hela.X/main.c
@@ -94,6 +94,9 @@ uint8_t I2C_Read(uint8_t ack)
 /********************** end I2C functions **********************/
 
 /********************** RTC chip functions *********************/
+// variables declaration
+
+
 // convert BCD to decimal function
 uint8_t bcd_to_decimal(uint8_t number) {
   return((number >> 4) * 10 + (number & 0x0F));
@@ -104,6 +107,39 @@ uint8_t decimal_to_bcd(uint8_t number) {
   return(((number / 10) << 4) + (number % 10));
 }
 
+uint8_t  second, minute, hour, m_day, month, year;
+
+// display time and date function
+void RTC_time_date_conversion()
+{
+  static char Time[] = "TIME: 00:00:00";
+  static char Date[] = "DATE: 00/00/2000";
+ 
+  // convert data from BCD format to decimal format
+  second = bcd_to_decimal(second);
+  minute = bcd_to_decimal(minute);
+  hour   = bcd_to_decimal(hour);
+  m_day  = bcd_to_decimal(m_day);
+  month  = bcd_to_decimal(month);
+  year   = bcd_to_decimal(year);
+  // end conversion
+ 
+  // update time
+  Time[6]  = hour   / 10 + '0';
+  Time[7]  = hour   % 10 + '0';
+  Time[9]  = minute / 10 + '0';
+  Time[10] = minute % 10 + '0';
+  Time[12] = second / 10 + '0';
+  Time[13] = second % 10 + '0';
+  // update date
+  Date[6]  = m_day  / 10 + '0';
+  Date[7]  = m_day  % 10 + '0';
+  Date[9]  = month  / 10 + '0';
+  Date[10] = month  % 10 + '0';
+  Date[14] = year   / 10 + '0';
+  Date[15] = year   % 10 + '0';
+ 
+}
 
 
 /********************** END RTC chip functions *********************/
